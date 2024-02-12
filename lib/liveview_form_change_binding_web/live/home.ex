bb51fd9069500e170f66dbdbddfc78b912d77ce6
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-12 09:52:33 +0800

    setup change binding

--- lib/liveview_form_change_binding_web/live/home.ex
+++ lib/liveview_form_change_binding_web/live/home.ex
@@ -4,14 +4,21 @@ defmodule LiveviewFormChangeBindingWeb.Home do
   def render(assigns) do
     ~H"""
     <h1>Welcome to Phoenix!</h1>
-    <form>
+    <form phx-change="change">
       <input type="number" name="length" value={@length} />
     </form>
+    <p>The length is <%= @length %></p>
     """
   end
 
   def mount(_params, _session, socket) do
     socket = assign(socket, length: 1)
     {:ok, socket}
+  end
+
+  def handle_event("change", _params, socket) do
+    length = socket.assigns.length + 1
+    socket = assign(socket, length: length)
+    {:noreply, socket}
   end
 end
